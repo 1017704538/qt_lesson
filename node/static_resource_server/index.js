46f31da77798781447b24933d64900a0439383f2
yuki <1017704538@qq.com>
2019-09-05 22:36:12 +0800

    node

---
+++ node/static_resource_server/index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+  const reqUrl = req.url;
+  // ^ /static/
+  if (/^\/static\//.test(reqUrl)) {
+    staticServer(req, res);
+    return false;
+  }
+  fs.readFile('./index.html', 'binary',
+    (err, file) => {
+      res.write(file, 'binary'),
+        res.end();
+    })
+})
+  .listen(7070, () => {
+    console.log('server is running 7070');
+  });
+function staticServer(req, res) {
+  const reqUrl = req.url;
+  // /static/demo,jpg . ..
+  // /static/tianshi.png
+  const filePath = path
+  .join(__dirname, reqUrl);
+  fs.exists(filePath, exists => {
+    if (!exists) {
+      res.writeHead(404);
+      res.end();
+      return false;
+    }
+    fs.readFile(filePath, 'binary',
+    (err, file) => {
+      res.write(file, 'binary');
+      res.end();
+    })
+  })
+}
